9e08d28effbc2cca9c2f0cf714a8383f7a1dba83
Kevin <kevin.cruz@hetic.net>
2020-11-30 21:02:32 +0100

    Plug du fichier Vestiaire

---
+++ Salle/Vestiare.py
@@ -0,0 +1,17 @@
+from Modele_Salle import Modele_Salle
+
+    #Elle peut etre la premiere salle
+
+def Vestiaire():
+    Vestiaire = Modele_Salle("Vestaire")
+    print("Le", Vestiaire.get_nom(),"est sombre, vous n'y voyez rien. Vous essayez de marché dans le noir lorsque vous butez sur objet. Mais quel est-il ?\n " )
+    print("TO DO : fonction Lampe")
+    print("TO DO : fonction Inventaire")
+    print("Vous pouvez maintenant voir. Mais... Un horrible spectacle est devant vous, du sang.. Du sang partout et des traces de griffure")
+    print("TO DO : fonction event")
+    print("TO DO : fonction Monstre")
+    print("TO DO : fonction Combat")
+
+
+
+Vestiaire()
