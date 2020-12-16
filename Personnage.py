4c2e57cc2590f6d53f604734b5845be084583567
Kevin <kevin.cruz@hetic.net>
2020-12-16 15:24:52 +0100

    Perso

--- Personnage.py
+++ Personnage.py
@@ -16,7 +16,6 @@ class Hero(Modele_Personnage, Modele_Arme, Modele_Armure):
     
 
     def recap(self):
-    #Pb d'heredité
        print("Statistiques:\nNom : ", self.get_nomPersonnage(), " | Vie : ", self.get_viePersonnage(), " | Attaque : ", self.get_attaquePersonnage(), " | Defense : ", self.get_defensePersonnage(), " | Niveau : ", self.get_levelPersonnage(), " | Arme : ", self.arme.get_nomArme(), "| Armure : ", self.armure.get_nomArmure(), "\n")   
 
 
