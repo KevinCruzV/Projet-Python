6a57140e202f115278136914439b02441f9fd7f1
Kevin <kevin.cruz@hetic.net>
2021-01-17 15:40:35 +0100

    Debug

---
+++ DEADSPACE.py
@@ -0,0 +1,15 @@
+from main_menu import*
+#nom du jeu a definir mdrr j'ai mis ça comme ça
+def DeadSpace():
+    print("\n")
+    print("DEAD SPACE\n")
+    print("Bienvenue dans le menu :")
+    sleep(2)
+    menu()
+
+
+
+
+
+
+DeadSpace()
